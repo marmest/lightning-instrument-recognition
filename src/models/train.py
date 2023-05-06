f23ff50a3fe607316d98b2be090ab42dd5de0475
Marin Meštrović <marin.mestrovic2612@gmail.com>
2023-05-06 09:51:08 +0200

    Small fix in train.py

--- src/models/train.py
+++ src/models/train.py
@@ -23,29 +23,53 @@ models = cfg.training.models
 
         
 if __name__ == '__main__':
-    X_train_mel = torch.load('../../data/processed/X_train_mel.pt')
-    X_val_mel = torch.load('../../data/processed/X_val_mel.pt')
-    y_train = torch.load('../../data/processed/y_train.pt')
-    y_val = torch.load('../../data/processed/y_val.pt')
-    train_dataset = TensorDataset(X_train_mel, y_train)
-    val_dataset = TensorDataset(X_val_mel, y_val)
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=8, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=8, shuffle=False)
 
     if "mel" in models:
+
+        X_train_mel = torch.load('../../data/processed/X_train_mel.pt')
+        X_val_mel = torch.load('../../data/processed/X_val_mel.pt')
+        y_train = torch.load('../../data/processed/y_train.pt')
+        y_val = torch.load('../../data/processed/y_val.pt')
+        train_dataset = TensorDataset(X_train_mel, y_train)
+        val_dataset = TensorDataset(X_val_mel, y_val)
+
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=8, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=8, shuffle=False)
+        
         model = CNN_mel(lr, num_labels)
         trainer = L.Trainer(callbacks=[RichProgressBar(leave=True), ModelCheckpoint(monitor='val_accuracy', save_top_k=1, dirpath='../../models/', filename='cnn_mel')],
                             max_epochs=epochs, fast_dev_run=False)
         trainer.fit(model, train_loader, val_loader)
 
     if "modgd" in models:
+
+        X_train_modgd = torch.load('../../data/processed/X_train_modgd.pt')
+        X_val_modgd = torch.load('../../data/processed/X_val_modgd.pt')
+        y_train = torch.load('../../data/processed/y_train.pt')
+        y_val = torch.load('../../data/processed/y_val.pt')
+        train_dataset = TensorDataset(X_train_modgd, y_train)
+        val_dataset = TensorDataset(X_val_modgd, y_val)
+
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=8, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=8, shuffle=False)
+
         model = CNN_modgd(lr, num_labels)
         trainer = L.Trainer(callbacks=[RichProgressBar(leave=True), ModelCheckpoint(monitor='val_accuracy', save_top_k=1, dirpath='../../models/', filename='cnn_modgd')],
                             max_epochs=epochs, fast_dev_run=False)
         trainer.fit(model, train_loader, val_loader)
 
     if "pitch" in models:
+
+        X_train_pitch = torch.load('../../data/processed/X_train_pitch.pt')
+        X_val_pitch = torch.load('../../data/processed/X_val_pitch.pt')
+        y_train = torch.load('../../data/processed/y_train.pt')
+        y_val = torch.load('../../data/processed/y_val.pt')
+        train_dataset = TensorDataset(X_train_pitch, y_train)
+        val_dataset = TensorDataset(X_val_pitch, y_val)
+
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=8, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=8, shuffle=False)
+
         model = CNN_pitch(lr, num_labels)
         trainer = L.Trainer(callbacks=[RichProgressBar(leave=True), ModelCheckpoint(monitor='val_accuracy', save_top_k=1, dirpath='../../models/', filename='cnn_pitch')],
                             max_epochs=epochs, fast_dev_run=False)
